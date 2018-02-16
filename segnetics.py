742b9ab2510d17749f0355fd2e0777c79d0f9839
cheyuriy <yrii167167@gmail.com>
2018-02-16 23:33:06 +0300

    parser for segnetics config

---
+++ segnetics.py
@@ -0,0 +1,108 @@
+import pprint
+import os
+from arpeggio import *
+from arpeggio import RegExMatch as _
+
+#Segnetics config grammar
+def segnetics_file(): return ZeroOrMore([comment_entry, request_entry]), EOF
+def comment_entry(): return _(r'^;.+')
+def request_entry(): return _(r'^\[.+\]'), direction_param, type_param, baudrate_param, address_param, period_param, OneOrMore(var_param)
+def direction_param(): return "Direction=", direction_value
+def direction_value(): return _(r'read/write|read')
+def type_param(): return "Type=", type_value
+def type_value(): return _(r'bit|reg')
+def baudrate_param(): return "Baudrate=", _(r'\d+')
+def address_param(): return "Address=", _(r'\d+')
+def period_param(): return "Period=", _(r'\d+')
+def var_param(): return _(r'var\d+='), var_address_value, "#", var_type_value, "#", var_name_value
+def var_address_value(): return _(r'[0-9a-f]+')
+def var_type_value(): return _(r'bool|long|int|real')
+def var_name_value(): return _(r'.+')
+
+
+class SegneticsVisitor(PTNodeVisitor):
+
+    def visit_var_name_value(self, node, children):
+        return re.sub("\r", "", node.value)
+
+    def visit_var_type_value(self, node, children):
+        return node.value
+
+    def visit_var_address_value(self, node, children):
+        return int(node.value, 16)
+
+    def visit_var_param(self, node, children):
+        return (children.var_name_value[0], 
+                dict(type=children.var_type_value[0], 
+                     name=children.var_name_value[0],
+                     addr=children.var_address_value[0]))
+    
+    def visit_period_param(self, node, children):
+        return None
+
+    def visit_address_param(self, node, children):
+        return None
+
+    def visit_baudrate_param(self, node, children):
+        return None
+
+    def visit_type_value(self, node, children):
+        return node.value
+
+    def visit_type_param(self, node, children):
+        return children.type_value[0]
+
+    def visit_direction_value(self, node, children):
+        return node.value
+
+    def visit_direction_param(self, node, children):
+        return children.direction_value[0]
+
+    def visit_request_entry(self, node, children):
+        request_type = None
+        type_param = children.type_param[0]
+        direction_param = children.direction_param[0]
+        if (type_param == "bit" and direction_param == "read"):
+            request_type = "discrete"
+        if (type_param == "bit" and direction_param == "read/write"):
+            request_type = "coil"
+        if (type_param == "reg" and direction_param == "read"):
+            request_type = "input"
+        if (type_param == "reg" and direction_param == "read/write"):
+            request_type = "holding"
+        return (request_type, children.var_param)
+
+    def visit_comment_entry(self, node, children):
+        return None
+
+    def visit_segnetics_file(self, node, children):
+        return children
+
+
+def parse(content):
+    parser = ParserPython(segnetics_file, reduce_tree=True)
+    parse_tree = visit_parse_tree(parser.parse(content), SegneticsVisitor())
+    return parse_tree
+
+
+def main(file_name=None):
+    if not file_name:
+        file_name = os.path.join(os.path.dirname(__file__),
+                                 'test.map')
+
+    with codecs.open(file_name, "r", encoding="cp1251") as opened_file:
+        opened_file_content = opened_file.read()
+
+    parse_tree = parse(opened_file_content)
+
+    return parse_tree
+
+
+if __name__ == "__main__":
+    # First parameter is segnetics modbus configuration file
+    if len(sys.argv) > 1:
+        entries = main(file_name=sys.argv[1])
+        pp = pprint.PrettyPrinter(indent=4)
+        pp.pprint(entries)
+    else:
+        print("Usage: python bibtex.py file_to_parse")
